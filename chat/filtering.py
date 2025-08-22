edfbfee1967467c033714c7fc0e7b437d897b4fa
sj <chris123ag@naver.com>
2025-08-22 17:55:59 +0900

    model load with hunggingface

--- chat/filtering.py
+++ chat/filtering.py
@@ -4,11 +4,20 @@ import os
 from django.conf import settings
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification,AutoTokenizer, AutoModelForSequenceClassification
 
-model_path = os.path.join(os.path.dirname(__file__), 'Add_beomi_model.pt')
-model = AutoModelForSequenceClassification.from_pretrained('beomi/kcbert-base')
-model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-model.eval()
-tokenizer = AutoTokenizer.from_pretrained('beomi/kcbert-base')
+# Hugging Face Hub에 올린 모델 경로
+model_name = "ksj1234/rangers_model"
+
+# 모델과 토크나이저 불러오기
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model.eval()  # 평가 모드로 설정
+
+# model_path = os.path.join(os.path.dirname(__file__), 'Add_beomi_model.pt')
+# model = AutoModelForSequenceClassification.from_pretrained('beomi/kcbert-base')
+# model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+# model.eval()
+# tokenizer = AutoTokenizer.from_pretrained('beomi/kcbert-base')
 
 # 새로운 문장을 분류하는 함수를 정의합니다.
 def classify_sentence(sentence):
